8379b4a1e66814efc6fb658324b8591253dd6f72
jaharul53 <100211147+jaharul53@users.noreply.github.com>
2024-12-09 11:34:55 +0600

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const connectDB = require("./config/db");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000; // Use process.env.PORT, and fallback to 3000 locally
 
 // Database connection
 connectDB();
@@ -13,18 +13,13 @@ connectDB();
 // Middleware
 app.use(express.json());
 
-
 // Routes
 app.use("/api/admin", require("./routes/adminRoutes"));
-
 app.use("/api/trainer", require("./routes/trainerRoutes"));
-
 app.use("/api/trainee", require("./routes/traineeRoutes"));
 
 // Error handling middleware
 app.use(require("./middlewares/errorHandler"));
-
-
 
 // Server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
